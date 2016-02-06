8f9f4de87af31836e648745dc898fa85621960ad
Richard Zhao <richardz@andrew.cmu.edu>
2016-02-06 01:59:00 -0500

    Change dbinit to python

--- db_init.sh
+++ db_init.sh
@@ -2,7 +2,8 @@
 
 rm -f app.db
 rm -rf migrations
-python3 run.py db init
-python3 run.py db migrate
-python3 run.py db upgrade
-python3 run.py db_populate
+python run.py db init
+python run.py db migrate
+python run.py db upgrade
+python run.py db_populate
+
